e0a2c77a7a51a25aabfaeb1994e8ea06076d7acf
Sudarshan Dubaga <sudarshandubaga@gmail.com>
2026-02-17 18:45:11 +0530

    profile

--- app/Models/Library.php
+++ app/Models/Library.php
@@ -15,4 +15,9 @@ class Library extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function plan()
+    {
+        return $this->belongsTo(SubscriptionPlan::class, 'subscription_plan_id');
+    }
 }
