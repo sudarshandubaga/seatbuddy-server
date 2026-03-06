9b130136caef0ea1e8ad1fccd13513bd3346aae2
Sudarshan Dubaga <sudarshandubaga@gmail.com>
2026-03-06 13:59:22 +0530

    attendance model

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 
 class Attendance extends Model
 {
-    //
+    use HasUuids;
+
+    protected $guarded = [];
+
+    protected $casts = [
+        'date' => 'date',
+    ];
 }
